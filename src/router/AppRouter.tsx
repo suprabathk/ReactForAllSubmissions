1a0302b7f7305a35d5cd3e15b30fd7de7f91aa9f
suprabathk <suprabathkondapally@gmail.com>
2023-05-19 19:44:40 +0530

    Added Lazy loading to Home page

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,16 +2,25 @@ import { Redirect, useRoutes } from "raviger";
 import About from "../components/About";
 import AppContainer from "../AppContainer";
 import Form from "../components/Form";
-import Home from "../components/Home";
 import { PreviewForm } from "../components/PreviewForm";
 import Page404 from "../components/Page404";
 import Login from "../components/Login";
 import { User } from "../types/userTypes";
+import { Suspense, lazy } from "react";
+
+const Home = lazy(() => import("../components/Home"));
 
 export default function AppRouter({ currentUser }: { currentUser: User }) {
   const loggedIn = currentUser?.username.length > 0;
   const routes = {
-    "/": () => (loggedIn ? <Home /> : <Redirect to="/login" />),
+    "/": () =>
+      loggedIn ? (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Home />
+        </Suspense>
+      ) : (
+        <Redirect to="/login" />
+      ),
     "/login": () => <Login />,
     "/about": () => (loggedIn ? <About /> : <Redirect to="/login" />),
     "/form/:id": ({ id }: { id: string }) =>
