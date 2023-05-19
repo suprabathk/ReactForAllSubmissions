317c7489f6ca9d0c6e439ff034800a2e22c11e6b
suprabathk <suprabathkondapally@gmail.com>
2023-05-19 18:59:36 +0530

    Added Sentry for production

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import * as Sentry from "@sentry/react";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: "https://3c8cc7bdde3a4cc6b1a071e9107c228f@o4505210851950592.ingest.sentry.io/4505210854244352",
+    integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+}
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
