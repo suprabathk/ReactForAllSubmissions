bf5fd02a9dfc4e68693e202621662e7474aac928
suprabathk <suprabathkondapally@gmail.com>
2023-05-20 22:03:12 +0530

    Displaying Login page instead of Redirecting to /login in Session router

--- src/router/SessionRouter.tsx
+++ src/router/SessionRouter.tsx
@@ -1,10 +1,10 @@
-import { Redirect, useRoutes } from "raviger";
+import { useRoutes } from "raviger";
 import Login from "../components/Login";
 
 export default function SessionRouter() {
   const routes = {
     "/login": () => <Login />,
   };
-  let routeResult = useRoutes(routes) || <Redirect to="/login" />;
+  let routeResult = useRoutes(routes) || <Login />;
   return routeResult;
 }
